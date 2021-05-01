8bcf73a83d2af24f144474d302e9db322a783a06
Dominik Kremer <dominik.kremer@gmx.de>
2021-05-01 14:11:48 +0200

    feat: add dispatchedTimes matcher

--- projects/jasmine-demo/src/app/book/book.service.action.spec.ts
+++ projects/jasmine-demo/src/app/book/book.service.action.spec.ts
@@ -18,7 +18,7 @@ describe('BookService', () => {
     expect(service).toBeTruthy();
   });
 
-  describe('addBook', () => {
+  describe('addBook simple', () => {
     it('should dispatch an instance of AddBook', () => {
       service.addBook(buildBook());
       expect(jasmine.any(AddBook)).toHaveBeenDispatched();
@@ -40,7 +40,39 @@ describe('BookService', () => {
     });
   });
 
-  describe('addFirstLaw', () => {
+  describe('addBook times', () => {
+    it('should dispatch an instance of AddBook once', () => {
+      service.addBook(buildBook());
+      expect(jasmine.any(AddBook)).toHaveBeenDispatchedTimes(1);
+    });
+
+    it('should not dispatch an instance of AddBook twice', () => {
+      service.addBook(buildBook());
+      expect(jasmine.any(AddBook)).not.toHaveBeenDispatchedTimes(2);
+    });
+
+    it('should dispatch a specific AddBook instance once', () => {
+      service.addBook(buildBook());
+      expect(new AddBook(buildBook())).toHaveBeenDispatchedTimes(1);
+    });
+
+    it('should not dispatch a specific AddBook instance twice', () => {
+      service.addBook(buildBook());
+      expect(new AddBook(buildBook())).not.toHaveBeenDispatchedTimes(2);
+    });
+
+    it('should not dispatch an instance of DeleteBook once', () => {
+      service.addBook(buildBook());
+      expect(jasmine.any(DeleteBook)).not.toHaveBeenDispatchedTimes(1);
+    });
+
+    it('should not dispatch a specific DeleteBook instance once', () => {
+      service.addBook(buildBook());
+      expect(new DeleteBook(buildBook())).not.toHaveBeenDispatchedTimes(1);
+    });
+  });
+
+  describe('addFirstLaw simple', () => {
     it('should dispatch an instance of AddBook', () => {
       service.addFirstLaw();
       expect(jasmine.any(AddBook)).toHaveBeenDispatched();
@@ -79,6 +111,43 @@ describe('BookService', () => {
     it('should not dispatch an instance of DeleteBook', () => {
       service.addFirstLaw();
       expect(jasmine.any(DeleteBook)).not.toHaveBeenDispatched();
+    });
+  });
+
+  describe('addFirstLaw times', () => {
+    it('should not dispatch an instance of AddBook only once', () => {
+      service.addFirstLaw();
+      expect(jasmine.any(AddBook)).not.toHaveBeenDispatchedTimes(1);
+    });
+
+    it('should dispatch an instance of AddBook thrice', () => {
+      service.addFirstLaw();
+      expect(jasmine.any(AddBook)).toHaveBeenDispatchedTimes(3);
+    });
+
+    it('should dispatch an AddBook instance for the first book once', () => {
+      service.addFirstLaw();
+      expect(
+        new AddBook({
+          title: 'Before They Are Hanged',
+          author: 'Joe Abercrombie',
+        })
+      ).toHaveBeenDispatchedTimes(1);
+    });
+
+    it('should not dispatch an AddBook instance for the first book thrice', () => {
+      service.addFirstLaw();
+      expect(
+        new AddBook({
+          title: 'Before They Are Hanged',
+          author: 'Joe Abercrombie',
+        })
+      ).not.toHaveBeenDispatchedTimes(3);
+    });
+
+    it('should not dispatch an instance of DeleteBook thrice', () => {
+      service.addFirstLaw();
+      expect(jasmine.any(DeleteBook)).not.toHaveBeenDispatchedTimes(3);
     });
   });
 
--- projects/ngxs-testing/src/lib/jasmine-matchers.ts
+++ projects/ngxs-testing/src/lib/jasmine-matchers.ts
@@ -4,37 +4,80 @@ declare global {
   namespace jasmine {
     interface Matchers<T> {
       toHaveBeenDispatched(): boolean;
+      toHaveBeenDispatchedTimes(times: number): boolean;
     }
   }
 }
 
-export const NgxsMatchers: jasmine.CustomMatcherFactories = {
-  toHaveBeenDispatched: (
-    util: jasmine.MatchersUtil,
-    customEqualityTester: readonly jasmine.CustomEqualityTester[]
-  ): jasmine.CustomMatcher => ({
-    compare: (action: any): jasmine.CustomMatcherResult => {
-      if (
-        util.contains(
-          ActionStorage.getAllActions(),
-          action,
-          customEqualityTester
-        )
-      ) {
-        return {
-          pass: true,
-          message: 'Dispatched',
-        };
-      } else {
-        return {
-          pass: false,
-          message: 'Not dispatched',
-        };
-      }
-    },
-  }),
-};
+const toHaveBeenDispatched = (
+  util: jasmine.MatchersUtil,
+  customTesters: readonly jasmine.CustomEqualityTester[]
+): jasmine.CustomMatcher => ({
+  compare: (action: any): jasmine.CustomMatcherResult => {
+    if (util.contains(ActionStorage.getAllActions(), action, customTesters)) {
+      return {
+        pass: true,
+        message: util.buildFailureMessage(
+          'to have been dispatched',
+          true,
+          action
+        ),
+      };
+    } else {
+      return {
+        pass: false,
+        message: util.buildFailureMessage(
+          'to have been dispatched',
+          false,
+          action
+        ),
+      };
+    }
+  },
+});
+
+const toHaveBeenDispatchedTimes = (
+  util: jasmine.MatchersUtil,
+  customTesters: readonly jasmine.CustomEqualityTester[]
+): jasmine.CustomMatcher => ({
+  compare: (action: any, expected: number): jasmine.CustomMatcherResult => {
+    const matchingActions = ActionStorage.getAllActions().filter((a) =>
+      util.equals(a, action, customTesters)
+    );
+    if (matchingActions.length === expected) {
+      return {
+        pass: true,
+        message: buildTimesFailureMessage(
+          util.pp(action),
+          true,
+          matchingActions.length,
+          expected
+        ),
+      };
+    } else {
+      return {
+        pass: false,
+        message: buildTimesFailureMessage(
+          util.pp(action),
+          false,
+          matchingActions.length,
+          expected
+        ),
+      };
+    }
+  },
+});
+
+const buildTimesFailureMessage = (
+  ppAction: string,
+  isNot: boolean,
+  actualTimes: number,
+  expectedTimes: number
+): string =>
+  `Expected ${ppAction}${
+    isNot ? ' not' : ''
+  } to have been dispatched ${expectedTimes} times. It was dispatched ${actualTimes} times.`;
 
 beforeAll(() => {
-  jasmine.addMatchers(NgxsMatchers);
+  jasmine.addMatchers({ toHaveBeenDispatched, toHaveBeenDispatchedTimes });
 });
